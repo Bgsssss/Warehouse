bd41dfb4470b791f7d5d64224984d8f6a5e0f333
Bagas Saputra <bagassaput48@gmail.com>
2025-11-23 23:53:55 +0700

    Delete warehouse-management directory

--- warehouse-management/src/main/java/com/warehouse/service/PelangganService.java
+++
@@ -1,8 +0,0 @@
-package com.warehouse.service;
-
-import com.warehouse.entity.Pelanggan;
-import com.warehouse.entity.Pemasok;
-
-public interface PelangganService {
-    Pelanggan cariPelangganById(Integer id);
-}
--- warehouse-management/src/main/java/com/warehouse/service/PersediaanService.java
+++
@@ -1,9 +0,0 @@
-package com.warehouse.service;
-
-import com.warehouse.entity.Persediaan;
-
-public interface PersediaanService {
-
-    void updateStokById(String idProduk,int jumlahDipesan, String idGudang);
-    Persediaan findByProdukAndGudang(String idProduk, String idGudang);
-}
